da9ef50762e61e4c93876e3bb1193cc0931cecb9
PedroGrou <pedrogroug@gmail.com>
2021-07-25 11:33:49 +0100

    first commit

---
+++ src/Components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <div className="section-main-title">
+      <h1>Pagination</h1>
+      <div className="underline"></div>
+    </div>
+  );
+}
+
+export default Title;
